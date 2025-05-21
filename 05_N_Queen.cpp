8ad52b93445eda5d0a515cc595552c35cf4a1a02
RAmPAtel007 <codewizard0007@gmail.com>
2025-05-22 02:18:47 +0530

    upadte

--- 05_N_Queen.cpp
+++ 05_N_Queen.cpp
@@ -85,3 +85,80 @@ int main(){
     return 0;
 
 }
+
+
+
+// through map 
+
+// class Solution {
+// public:
+// unordered_map<int, bool> rowCheck;
+// unordered_map<int,bool> upperLeftDiagonalCheck;
+// unordered_map<int,bool> bottomLeftDiagonalCheck;
+
+// void storeSoln(vector<vector<char>> &board, int n,vector<vector<string>> &ans){
+//     vector<string> temp;
+//     for(int i=0; i<n; i++){
+//         string output ="";
+//         for(int j=0;j<n;j++ ){
+//             output.push_back(board[i][j]);
+//         }
+//         temp.push_back(output);
+//     }
+//     ans.push_back(temp);
+// }
+
+// bool isSafe(int row, int col, vector<vector<char>> &board,int n){
+//     if(rowCheck[row]==true){
+//         return false;
+//     }
+//     if(upperLeftDiagonalCheck[n-1+col-row]== true){
+//         return false;
+
+//     }
+//     if(bottomLeftDiagonalCheck[row+col]== true){
+//         return false;
+//     }
+//     return true ;
+
+// }
+
+// void solve(vector<vector<char>>& board, int col, int n,vector<vector<string>>& ans){
+//     //base case 
+//     if(col >= n){ //saare queen place ho gaye hai or col==n bur good practice col>=n
+//         storeSoln(board,n,ans);
+//         return;
+
+//     }
+
+//     //1 case mera 
+//     for(int row=0 ; row<n ; row++){
+//         if(isSafe(row,col,board,n)){
+//             //rakh do 
+//             board[row][col]= 'Q';
+//             rowCheck[row] =true;
+//             upperLeftDiagonalCheck[n-1+col-row]= true;
+//             bottomLeftDiagonalCheck[row+col]=true;
+//             //recursion soln lagega 
+//             solve(board,col+1,n,ans);
+//             //backtracking 
+//             board[row][col]='.';
+//             rowCheck[row] =false;
+//             upperLeftDiagonalCheck[n-1+col-row]= false;
+//             bottomLeftDiagonalCheck[row+col]=false;
+
+//         }
+//     }
+// }
+//     vector<vector<string>> solveNQueens(int n) {
+//         vector<vector<char>> board(n,vector<char>(n,'.'));
+//         vector<vector<string>> ans;
+//     int col=0;
+//     //Q -> queen at the cell
+//     //- -> empty cell
+//     solve(board,col,n,ans);
+//     return ans;
+
+// }
+
+// };
